4a79024d3493b0dfb35018dcb8e54cb88ee006fb
JIN <a1824jj@aiit.ac.jp>
2020-05-03 07:41:38 +0000

    postpone process

--- session5/index.js
+++ session5/index.js
@@ -1,2 +1,13 @@
+//
 console.log(__dirname);
 console.log(__filename);
+
+//
+setTimeout( () => {
+  console.log("setTimeout()");
+}, 100);
+
+console.log("global");
+
+var end = (new Date()).getDate + 3000;
+while((new Date()).getTime() < end){}
