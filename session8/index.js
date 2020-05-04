053f4862dea147f1da924a3079bb4d9e1e92c806
JIN <a1824jj@aiit.ac.jp>
2020-05-04 02:29:40 +0000

    using EventEmitter sample

--- session8/index.js
+++ session8/index.js
@@ -1,4 +1,3 @@
-/*
 var Clock = require("./clock");
 var i = 0;
 var clock = new Clock();
@@ -10,36 +9,3 @@ clock.on("tick", () => {
 });
 
 clock.start();
-*/
-
-var EventEmitter = require("events");
-var ee = new EventEmitter();
-
-var ontick = function() {
-  console.log("event is called.");
-  ee.off("event", ontick);
-};
-
-ee.on("event", ontick);
-ee.emit("event");
-ee.emit("event");
-
-
-var ontick2 = function() {
-  console.log("event2 is called.");
-  ee.off("event2", ontick2);
-};
-ee.once("event2", ontick2);
-ee.emit("event2");
-ee.emit("event2");
-
-
-ee.on("event3", function(){
-  console.log("function : \r\n", this);
-});
-
-ee.on("event3", ()=>{
-  console.log("arrow : \r\n", this);
-});
-
-ee.emit("event3");
