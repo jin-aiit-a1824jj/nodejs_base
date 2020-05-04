1bfb8a27f71e3ba168999bbf6f17b5f6c0d695dc
JIN <a1824jj@aiit.ac.jp>
2020-05-04 03:42:46 +0000

    create custom stream - write

---
+++ session9/customwriter.js
@@ -0,0 +1,31 @@
+const { Writable } = require("stream");
+const randomstring = require("./randomstring");
+const data = randomstring();
+
+var CustomWriter = class extends Writable {
+
+  constructor(options){
+    super(options || {decodeStrings: true});
+  }
+
+  _write(chunk, encoding, done){
+    var text;
+
+    if(encoding === "buffer"){
+      text = chunk.toString(this._writableState.defaultEncoding);
+    }else{
+      text = chunk;
+    }
+    try {
+      console.log(text);
+    } catch (error) {
+      done(error);
+      return;
+    }
+    
+    done();
+  }
+
+}; 
+
+module.exports =  CustomWriter;
--- session9/index6.js
+++ session9/index6.js
@@ -7,3 +7,9 @@ reader.on("data", (chunk)=>{
   console.log(chunk);
 });
 reader.resume();
+
+var CustomWritter = require("./customwriter");
+var writer = new CustomWritter();
+var reader2 = new CustomReader();
+reader2.pipe(writer);
+reader2.resume();
