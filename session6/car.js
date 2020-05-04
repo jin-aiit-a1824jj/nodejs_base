2221c9cb1b36bb998e45e31fd20a8f46d407387e
JIN <a1824jj@aiit.ac.jp>
2020-05-04 01:20:44 +0000

    module - object, funtions, class

---
+++ session6/car.js
@@ -0,0 +1,13 @@
+var Car = class {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  drive() {
+    console.log("zoom zoom ...");
+  }
+
+};
+
+module.exports = Car;
--- session6/index.js
+++ session6/index.js
@@ -1,2 +1,14 @@
 var message = require("./modules");
 console.log(message);
+
+var {echo, area} = require("./methods");
+echo("Hello World! 2");
+console.log(area(10,5));
+
+var Lamborghini = require("./lamborgini");
+var car = new Lamborghini("lamborghini");
+car.echo();
+car.drive();
+
+var config = require("./config");
+console.log(JSON.stringify(config));
